b3cfacb262e0100ef52aa48c8dde16c4fa381585
apereverzin <andrey.pereverzin@hotmail.com>
2018-01-30 20:02:40 +0000

    Added tests

--- test/MapMessHappyPath.js
+++ test/MapMessHappyPath.js
@@ -1,17 +1,30 @@
 // Contract to be tested
 var MapMess = artifacts.require("./MapMess.sol");
 
-// Test suite
 contract('MapMess', function(accounts) {
   var chainListInstance;
   var seller = accounts[0];
 
-  // Test case: check initial values
-  it("should be initialized", function() {
+  it("contract should be initialized", function() {
+    var contractInstance;
+
     return MapMess.deployed().then(function(instance) {
-      return instance.getTokenPrice.call();
+      contractInstance = instance;
+      return contractInstance.getOwner.call();
     }).then(function(data) {
-      assert.equal(data, 0x1, "1token price should be ");
+      assert.equal(data, accounts[0], "owner should be " + accounts[0]);
+      return contractInstance.getTokenPrice.call();
+    }).then(function(data) {
+      assert.equal(data, 0x1, "token price should be 1");
+      return contractInstance.totalSupply.call();
+    }).then(function(data) {
+      assert.equal(data, 10000, "total supply should be 10000");
+      return contractInstance.balanceOf(accounts[1]);
+    }).then(function(data) {
+      assert.equal(data, 0x0, "initial balance should be 0");
+      return contractInstance.getBalance.call();
+    }).then(function(data) {
+      assert.equal(data, 10000, "owner balance should be 10000");
     });
   });
 });
