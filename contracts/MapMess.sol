b0e420fc8c146be664b7548c2a04468a82b563b3
apereverzin <andrey.pereverzin@hotmail.com>
2018-03-10 21:59:57 +0000

    Fixed To comply with ERC20 - renamed event to Transfer

--- contracts/MapMess.sol
+++ contracts/MapMess.sol
@@ -1,23 +1,20 @@
 pragma solidity ^0.4.17;
 
 
-contract MapMess {
-    address public owner = msg.sender;
+import "./SafeMath.sol";
 
-    modifier onlyOwner {
-        if (msg.sender != owner) revert();
+
+contract MapMess {
+    address public owner;
+
+    using SafeMath for uint256;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
         _;
     }
 
-    function changeOwner(address _newOwner) public
-    onlyOwner
-    {
-        if (_newOwner == 0x0) revert();
-        owner = _newOwner;
-    }
-
-    function kill() public {
-        if (msg.sender == owner)
+    function kill() public onlyOwner {
         selfdestruct(owner);
     }
 
@@ -31,58 +28,45 @@ contract MapMess {
 
     mapping (address => uint256) balances;
 
-    mapping (address => mapping (address => uint256)) allowed;
-
     function MapMess(
     string _name,
     string _symbol,
     uint256 _totalSupply) public {
+        owner = msg.sender;
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply;
         balances[msg.sender] = _totalSupply;
     }
 
-    function() public payable {
-        //
-    }
-
-    function totalSupply() constant public returns (uint256) {
+    function totalSupply() public view returns (uint256) {
         return totalSupply;
     }
 
-    function balanceOf(address _owner) constant public returns (uint256){
+    function balanceOf(address _owner) public view returns (uint256){
         return balances[_owner];
     }
 
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(balances[msg.sender] >= _value && _value > 0);
 
-        balances[msg.sender] = balances[msg.sender] - _value;
-        balances[_to] = balances[_to] + _value;
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
 
-        tokensTransferred(msg.sender, _to, _value);
+        Transfer(msg.sender, _to, _value);
         return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value)
     public onlyOwner returns (bool) {
-        balances[_from] = balances[_from] - _value;
-        balances[_to] = balances[_to] + _value;
-        allowed[_from][msg.sender] = allowed[_from][msg.sender] - _value;
+        require(balances[_from] >= _value && _value > 0);
 
-        tokensTransferred(_from, _to, _value);
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        Transfer(_from, _to, _value);
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool){
-        allowed[msg.sender][_spender] = _value;
-
-        tokensTransferApproved(msg.sender, _spender, _value);
-        return true;
-    }
-
-    event tokensTransferred(address indexed _from, address indexed _to, uint256 _value);
-
-    event tokensTransferApproved(address indexed _owner, address indexed _spender, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 }
