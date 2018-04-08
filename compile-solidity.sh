8a9e3d10be13edad8cf7156a05f8b2e7dc5171a7
apereverzin <andrey.pereverzin@hotmail.com>
2018-04-08 17:56:10 +0100

    Used SafeMath

--- compile-solidity.sh
+++ compile-solidity.sh
@@ -5,12 +5,15 @@ function solc-err-only {
 }
 
 solc-err-only --overwrite --optimize --bin --abi ./contracts/MapMess.sol -o ./build/
-#solc --link --libraries ./contracts/MapMess.sol:SafeMath:0x1c0616e7b6d742add1cd3063ece0996edad3f32a ./build/MapMess.bin
-solc --link --libraries ./contracts/MapMess.sol:SafeMath:0x296585e8f9f1b15916ee16a25b9931b9132049d5 ./build/MapMess.bin
-#solc --link --libraries ./contracts/MapMess.sol:SafeMath:0x1c0616e7b6d742add1cd3063ece0996edad3f32a ./build/MapMess.bin
-cd ./build
-wc -c MapMess.bin | awk '{print "MapMess: " $1}'
 
 #local 0x1c0616e7b6d742add1cd3063ece0996edad3f32a
+#solc --link --libraries ./contracts/MapMess.sol:SafeMath:0x1c0616e7b6d742add1cd3063ece0996edad3f32a ./build/MapMess.bin
+
 #Rinkeby 0x296585e8f9f1b15916ee16a25b9931b9132049d5
+solc --link --libraries ./contracts/MapMess.sol:SafeMath:0x296585e8f9f1b15916ee16a25b9931b9132049d5 ./build/MapMess.bin
+
 #Mainnet
+#solc --link --libraries ./contracts/MapMess.sol:SafeMath:0x1c0616e7b6d742add1cd3063ece0996edad3f32a ./build/MapMess.bin
+
+cd ./build
+wc -c MapMess.bin | awk '{print "MapMess: " $1}'
