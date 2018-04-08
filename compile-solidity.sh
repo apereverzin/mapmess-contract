deea11106b8be9e0b4902501f1164961a3e2bbef
apereverzin <andrey.pereverzin@hotmail.com>
2018-04-08 11:56:16 +0100

    Using SafeMath

--- compile-solidity.sh
+++ compile-solidity.sh
@@ -5,5 +5,12 @@ function solc-err-only {
 }
 
 solc-err-only --overwrite --optimize --bin --abi ./contracts/MapMess.sol -o ./build/
+#solc --link --libraries ./contracts/MapMess.sol:SafeMath:0x1c0616e7b6d742add1cd3063ece0996edad3f32a ./build/MapMess.bin
+solc --link --libraries ./contracts/MapMess.sol:SafeMath:0x296585e8f9f1b15916ee16a25b9931b9132049d5 ./build/MapMess.bin
+#solc --link --libraries ./contracts/MapMess.sol:SafeMath:0x1c0616e7b6d742add1cd3063ece0996edad3f32a ./build/MapMess.bin
 cd ./build
 wc -c MapMess.bin | awk '{print "MapMess: " $1}'
+
+#local 0x1c0616e7b6d742add1cd3063ece0996edad3f32a
+#Rinkeby 0x296585e8f9f1b15916ee16a25b9931b9132049d5
+#Mainnet
--- contracts/MapMess.sol
+++ contracts/MapMess.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.17;
 
-
 contract MapMess {
     address public owner;
+
+    using SafeMath for uint256;
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -45,8 +46,8 @@ contract MapMess {
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(balances[msg.sender] >= _value && _value > 0);
 
-        balances[msg.sender] = sub(balances[msg.sender], _value);
-        balances[_to] = add(balances[_to], _value);
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
 
         Transfer(msg.sender, _to, _value);
         return true;
@@ -56,23 +57,58 @@ contract MapMess {
     public onlyOwner returns (bool) {
         require(balances[_from] >= _value && _value > 0);
 
-        balances[_from] = sub(balances[_from], _value);
-        balances[_to] = add(balances[_to], _value);
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
 
         Transfer(_from, _to, _value);
         return true;
     }
 
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+}
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    /**
+    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) pure returns (uint256) {
         assert(b <= a);
         return a - b;
     }
 
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) pure returns (uint256) {
         uint256 c = a + b;
         assert(c >= a);
         return c;
     }
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 }
